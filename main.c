790ea1f512a49241d1134ee027c8c7905ce64b20
Thulani-Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-01-03 11:08:19 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+Creator: Thulani Ndlovu
+date: 03/01/2023
+Title: String reversal
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+
+printf("Hello World!");
+    return 0;
+}
